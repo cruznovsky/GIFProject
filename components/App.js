95a634b575fa0797fef9459145f90ae2f95a48f1
Jakub Sadowski <jaakub.sadowski@gmail.com>
2019-10-13 18:39:21 +0200

    Add promises to react app

--- components/App.js
+++ components/App.js
@@ -15,30 +15,32 @@ App = React.createClass({
         this.setState({
             loading: true
         });
-        this.getGif(searchingText, function (gif) {
+        this.getGif(searchingText).then(gif => {
             this.setState({
                 loading: false,
                 gif: gif,
                 searchingText: searchingText
             });
-        }.bind(this));
+        })
     },
 
-    getGif: function (searchingText, callback) {
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);
-            }
-        };
-        xhr.send();
+    getGif: function (searchingText) {
+        return new Promise((res, rej) => {
+            var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText).data;
+                    var gif = {
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    };
+                    res(gif);
+                }
+            };
+            xhr.send();
+        })
     },
 
     render: function () {
